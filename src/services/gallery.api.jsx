e6eefa2db8d677b2a0f60dfcfb74833aac9ef771
Anna <anjanadvorna@gmail.com>
2023-11-22 17:54:01 +0200

    Update gallery.api.jsx

--- src/services/gallery.api.jsx
+++ src/services/gallery.api.jsx
@@ -1,16 +1,12 @@
 export default function fetchGallery(searchQuery, page) {
+  
   const API_KEY = '39912863-1650dbe31ef88f10e118c8e6a';
-  const URL = `https://pixabay.com/api/?
-    q=${searchQuery}&
-    page=${page}&
-    key=${API_KEY}&
-    image_type=photo&
-    orientation=horizontal&
-    per_page=12`
+  const URL = `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
 
   return fetch(URL).then(response => 
     response.json()
-  )    
+  )
+    
 }
 
 
